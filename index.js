48cf17fea44eb6e0d33473ef156e894b4eb0bca8
Dung24-6 <hadung24062002@gmail.com>
2023-01-03 20:55:47 +0700

    set up database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +20,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
