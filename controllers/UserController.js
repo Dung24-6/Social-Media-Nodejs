21a3f382b7cc796136058b883a3d1aff02057a4b
Dung24-6 <hadung24062002@gmail.com>
2023-01-08 19:26:03 +0700

    Fix error handling , not use "throw"

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,54 +21,39 @@ const getALLUsers = async (req, res) => {
 };
 
 const registerUser = async (req, res) => {
-  // const { username, email, password } = req.body;
-  // if (!(username && email && password)) {
-  //   res.status(500).json("Not enough params");
-  // }
-  // const exitingUser = await UsersModel.findOne({ where: { email: email } });
-  // if (exitingUser) {
-  //   res.status(500).json("Email already in use");
-  // }
-  // try {
-  //   await UsersModel.create({
-  //     username,
-  //     email,
-  //     password,
-  //   });
-  // } catch (err) {
-  //   return res.status(500).json("Can't register");
+  const { username, email, password } = req.body;
+  if (!(username && email && password)) {
+    return res.status(400).json("Not enough params");
+  }
   try {
-    const { username, email, password } = req.body;
-    if (!(username && email && password)) {
-      throw new Error("Not enough params");
+    const exitingUser = await UsersModel.findOne({ where: { email: email } });
+    if (exitingUser) {
+      res.status(500).json("Email already in use");
+    } else {
+      const user = await UsersModel.create({
+        username,
+        email,
+        password,
+      });
+      return res.json(user);
     }
-    const existingUser = await UsersModel.findOne({ where: { email: email } });
-    if (existingUser) {
-      throw new Error("Email  must be unique");
-    }
-    const user = await UsersModel.create({
-      username,
-      email,
-      password,
-    });
-    return res.json("blablaok");
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
 };
 
 const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!(email && password)) {
+    return res.status(400).json("Missing input");
+  }
   try {
-    const { email, password } = req.body;
-    if (!(email && password)) {
-      throw new Error("Missing input");
-    }
     const user = await UsersModel.findOne({ where: { email: email } });
     if (!user) {
-      throw new Error("Email incorrect");
+      return res.status(400).json("Email incorrect");
     }
     if (password !== user.password) {
-      throw new Error("Password incorrect");
+      return res.status(400).json("Password incorrect");
     }
     return res.json("OK");
   } catch (err) {
